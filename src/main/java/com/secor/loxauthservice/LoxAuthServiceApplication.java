b2ffd98ce154131d7bb00bf346138c014f29534f
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2024-11-27 21:37:05 +0530

    Initial commit

---
+++ src/main/java/com/secor/loxauthservice/LoxAuthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.secor.loxauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoxAuthServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LoxAuthServiceApplication.class, args);
+    }
+
+}
